e46fde274fd13a5498ef18bc855aef7c8f7b75cd
Plunderer <kazutokun467@gmail.com>
2020-04-23 12:42:44 +0600

    ladder11

---
+++ 32B.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+ string s,s1;
+ cin>>s;
+
+for(int i=0;i<s.size();i++)
+
+{
+	if(s[i]=='.'){
+		s1+='0';
+	}
+	if(s[i]=='-'&&s[i+1]=='.'){
+		s1+='1';
+		i++;
+
+	}
+	if(s[i]=='-'&&s[i+1]=='-'){
+		s1+='2';
+		i++;
+	}
+
+}
+ cout<<s1;
+
+
+
+ 	return 0;
+}
